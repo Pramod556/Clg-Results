acbf720aa36e27e39dfeba1d17694e9221bac79b
Pramod Swargam <98465143+Pramod556@users.noreply.github.com>
2023-03-29 12:29:04 +0530

    fetched hall ticket number

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
-const http=require('http');
+const { json } = require('express');
 const express=require('express');
-const result=require('./results')
+// const result=require('./results')
+// const fetch = require('node-fetch');
 // const views=require('views');
 const app=express();
-app.listen(8080);
+app.use(express.urlencoded({extended:true}))
+app.listen(8080,()=>{
+    console.log("listening for requests")
+});
 app.set('view engine','ejs');
 app.get('/',(req,res)=>{
-    const obj=result;
-    res.render('hello',{result:result});
-    console.log('server',{result:result})
+   res.send("hello");
+})
+app.get('/data',(req,res)=>{
+    console.log("reached data")
+    res.render('form')
+})
+app.post('/form',(req,res)=>{
+    console.log('path')
+    let path=req.body.htnum;
+    console.log(path)
+    let url;
+    url='https://results-restapi.up.railway.app/all-r18/'+path;
+    console.log("fetch")
+     
+     
+    fetch(url).then((res=>res.json().then(data=>console.log(Object.keys(data).forEach(ele=>{
+console.log(ele)
+    }))))
+
+    ) 
+    // app.post("/", function(req, res) {
+    //     var num1 = Number(req.body.num1);
+    //     var num2 = Number(req.body.num2);
+          
+    //     var result = num1 + num2 ;
+          
+    //     res.send("Addition - " + result);
+    //   });
+
 })
